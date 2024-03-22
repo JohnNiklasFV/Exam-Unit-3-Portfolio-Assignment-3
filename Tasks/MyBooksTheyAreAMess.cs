0de5befc2ae18271624f637044e8373d658481d5
JohnNiklasFV <jnvenaasen@uia.no>
2024-03-22 23:46:10 +0100

    Task4/MyBooksTheyAreAMess. Done with the functions i was able to do

--- Tasks/MyBooksTheyAreAMess.cs
+++ Tasks/MyBooksTheyAreAMess.cs
@@ -17,9 +17,29 @@ public class Task4
 
         int count = CountBooksWrittenAfter1992(books);
         Console.WriteLine($"\nNumber of books written after 1992: {count}");
+
+        int count2004 = CountBooksWrittenBefore2004(books);
+        Console.WriteLine($"Number of books written before 2004: {count2004}");
+
+        List<string> isbnNumbers = GetIsbnNumbersForAuthor(books, "John Doe");
+        Console.WriteLine("\nISBN numbers of books written by John Doe:");
+        foreach (string isbn in isbnNumbers)
+        {
+            Console.WriteLine(isbn);
+        }
+
+
+        Console.WriteLine("\nList of books alphabetically ascending:");
+        ListBooksAlphabeticallyAscending(books);
+
+
+        Console.WriteLine("\nList of books chronologically ascending:");
+        ListBooksChronologicallyAscending(books);
     }
 
-    //Return only books that starts on The----------------------------------------------
+
+
+
     public void FilterByStartingWithThe(Book[] books)
     {
         foreach (Book book in books)
@@ -32,7 +52,8 @@ public class Task4
     }
 
 
-    //filter by T in the authors name--------------------------------------------------
+
+
     public void FilterByContainingT(Book[] books)
     {
         foreach (Book book in books)
@@ -43,7 +64,10 @@ public class Task4
             }
         }
     }
-    //filter by number of books written after 1992--------------------------------------
+
+
+
+
     public int CountBooksWrittenAfter1992(Book[] books)
     {
         int count = 0;
@@ -56,7 +80,64 @@ public class Task4
         }
         return count;
     }
+  
 
+
+
+    public int CountBooksWrittenBefore2004(Book[] books)
+    {
+        int count = 0;
+        foreach (Book book in books)
+        {
+            if (book.publication_year < 2004)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+
+    public List<string> GetIsbnNumbersForAuthor(Book[] books, string authorName)
+    {
+        List<string> isbnNumbers = new List<string>();
+        foreach (Book book in books)
+        {
+            if (book.author.Equals(authorName, StringComparison.OrdinalIgnoreCase))
+            {
+                isbnNumbers.Add(book.isbn);
+            }
+        }
+        return isbnNumbers;
+    }
+   
+
+
+
+    public void ListBooksAlphabeticallyAscending(Book[] books)
+    {
+        var sortedBooks = books.OrderBy(book => book.title, StringComparer.OrdinalIgnoreCase);
+        foreach (var book in sortedBooks)
+        {
+            Console.WriteLine($"Title: {book.title}, Publication Year: {book.publication_year}, Author: {book.author}, ISBN: {book.isbn}");
+        }
+    }
+  
+
+
+
+    public void ListBooksChronologicallyAscending(Book[] books)
+    {
+        var sortedBooks = books.OrderBy(book => book.publication_year);
+        foreach (var book in sortedBooks)
+        {
+            Console.WriteLine($"Title: {book.title}, Publication Year: {book.publication_year}, Author: {book.author}, ISBN: {book.isbn}");
+        }
+    }
+    
+    
 
     public class Book
     {
